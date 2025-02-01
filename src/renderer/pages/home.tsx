7dd4168ee05a99662131c5b491e87165691a2a55
Diego <dg456398@gmail.com>
2025-02-01 08:32:24 -0300

    ajustando xml

--- src/renderer/pages/home.tsx
+++ src/renderer/pages/home.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+import './hm.css';
+
 export default function Home() {
+  const [xml, setXml] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && file.type === 'text/xml') {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target?.result as string;
+        setXml(text);
+      };
+      console.log(xml);
+      reader.readAsText(file);
+    } else {
+      alert('Por favor, selecione um arquivo XML válido.');
+    }
+  };
+
   return (
-    <div>
-      <h2>Nova tela</h2>
+    <div className="container">
+      <input
+        type="file"
+        accept=".xml"
+        onChange={handleFileChange}
+        className="mb-4"
+      />
+      {xml && <textarea value={xml} readOnly rows={10} className="area" />}
     </div>
   );
 }
